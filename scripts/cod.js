2360d5c3229cac483b3047484b62a4419f77c7d9
Erimend <57846471+Erimend@users.noreply.github.com>
2019-11-23 18:42:58 -0600

    Rolls v1.0

--- scripts/cod.js
+++ scripts/cod.js
@@ -39,8 +39,63 @@ CONFIG.skills = {
   "subterfuge": "Subterfuge"
 };
 
+// Group Names
+CONFIG.groups = {
+    "mental": "Mental",
+	"physical": "Physical",
+	"social": "Social"
+}
+
+// Group Mapping
+CONFIG.groupMapping = { 
+  "int": "mental",
+  "wits": "mental",
+  "res": "mental",
+  "str": "physical",
+  "dex": "physical",
+  "sta": "physical",
+  "pre": "social",
+  "man": "social",
+  "com": "social",
+  "academics": "mental",
+  "computer": "mental",
+  "crafts": "mental",
+  "investigation": "mental",
+  "medicine": "mental",
+  "occult": "mental",
+  "politics": "mental",
+  "science": "mental",
+  "athletics": "physical",
+  "brawl": "physical",
+  "drive": "physical",
+  "firearms": "physical",
+  "larceny": "physical",
+  "stealth": "physical",
+  "survival": "physical",
+  "weaponry": "physical",
+  "animalken": "social",
+  "empathy": "social",
+  "expression": "social",
+  "intimidation": "social",
+  "persuasion": "social",
+  "socialize": "social",
+  "streetwise": "social",
+  "subterfuge": "social"
+}
+
 class ActorCoD extends Actor 
 {
+	rollPool(attribute, skill, modifier)
+{
+	let pool = 0
+	if (attribute != "none")
+    pool += this.data.data.attributes[attribute].current
+	if (skill != "none")
+    pool += this.data.data.skills[skill].current
+	pool += parseInt(modifier) || 0
+	let roll = new Roll(`${pool}d10x10cs>=8`).roll()
+    roll.toMessage()
+}
 }
 CONFIG.Actor.entityClass = ActorCoD
 
@@ -80,14 +135,86 @@ _prepareItems(actorData)
 	activateListeners(html) {
     super.activateListeners(html);
 
-	html.find(".skill-click").click(event => {
-	let int = this.actor.data.data.attributes.int.current
-    let academics = this.actor.data.data.skills.academics.current
-    let result = int + academics
-	console.log(result)
-	let roll = new Roll(`${result}d10x10cs>=8`).roll()
-	roll.toMessage()
+	html.find(".roll-pool").click(event => {
+	let defaultSelection = $(event.currentTarget).attr("data-skill")
+	let stats = duplicate(CONFIG.skills) 
+	let attributes = duplicate(CONFIG.attributes)
+	let skillGroups = duplicate(CONFIG.groups)
+	let attrGroups = duplicate(CONFIG.groups)
+
+	for (let g in skillGroups)
+	{
+		 skillGroups[g] = {}
+		 attrGroups[g] = {}
+	}
+
+	for (let s in stats)
+	{
+		let skillGroup = CONFIG.groupMapping[s]
+		skillGroups[skillGroup][s] = stats[s]
+	}
+		for (let a in attributes)
+	{
+		let attrGroup = CONFIG.groupMapping[a]
+		attrGroups[attrGroup][a] = attributes[a]
+	}
+	
+	let displaySkillGroups = Object.keys(skillGroups).map((key) => {
+  const newKey = CONFIG.groups[key];
+  return { [newKey] : skillGroups[key] };
+}).reduce((a, b) => Object.assign({}, a, b));
+
+let displayAttrGroups = Object.keys(attrGroups).map((key) => {
+  const newKey = CONFIG.groups[key];
+  return { [newKey] : attrGroups[key] };
+}).reduce((a, b) => Object.assign({}, a, b));
+
+for (let g in displaySkillGroups)
+{
+  for (let s in displaySkillGroups[g])
+  {
+    displaySkillGroups[g][s] += " (" + this.actor.data.data.skills[s].current+")"
+  }
+}
+
+for (let g in displayAttrGroups)
+{
+  for (let a in displayAttrGroups[g])
+  {
+    displayAttrGroups[g][a] += " (" + this.actor.data.data.attributes[a].current+")"
+  }
+}
+
+		let dialogData = {
+			defaultSelection : defaultSelection,
+			skills : displaySkillGroups,
+			attributes : displayAttrGroups,
+			groups : CONFIG.groups
+        }
+    renderTemplate('systems/cod/templates/pool-dialog.html', dialogData) .then(html => {
+            new Dialog({
+            title: "Roll Dice Pool",
+            content: html,
+            buttons: {
+              Yes: {
+                icon: '<i class="fa fa-check"></i>',
+                label: "Yes",
+				callback: (html) => {
+                    let attributeSelected = html.find('[name="attributeSelector"]').val();
+					let poolModifier = html.find('[name="modifier"]').val();
+					let skillSelected = html.find('[name="skillSelector"]').val();
+                    this.actor.rollPool(attributeSelected, skillSelected, poolModifier)
+                  }
+              },
+              cancel: {
+                icon: '<i class="fas fa-times"></i>',
+                label: "Cancel"
+              },
+            },
+            default: 'Yes'
+          }).render(true)
     })
+	})
 
     // Activate tabs
     let tabs = html.find('.tabs');
